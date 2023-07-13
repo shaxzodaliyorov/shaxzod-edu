d017868969450c1ad350161dffe8c4d4b14dafa8
shaxzod <shaxzodaliyorov0202@gmail.com>
2023-07-13 15:52:13 +0500

    seo-chnage

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -5,7 +5,7 @@ export const SiteConfig = {
 			"shaxzod aliyorov, dasturlash kurslari, dasturlashga oid darslar, reactjs uzbek tilida, vuejs uzbek tilida, redux uzbek tilida, Shaxzod , Shaxzod  academy, bepul dasturlash, rezyume yozish, portfolio, Shaxzod  javascript, Shaxzod  raqamli avlod, javascript, reactjs, vuejs, javascript darslari, reactjs darslari, vuejs darslari, dasturlash darslari, o'zbek tilida dasturlash, reactjs o'zbek tilida, reactjs darslari o'zbek tilida, javascript darslari, javascript darslari o'zbek tilida, dasturash darslari o'zbek tilida, dasturlashni o'rganish, dasturlash, IT loyihalar o'zbek tilida",
 		keyword:
 			"shaxzod aliyorov, dasturlash kurslari, dasturlashga oid darslar, reactjs uzbek tilida, vuejs uzbek tilida, redux uzbek tilida, Shaxzod , Shaxzod  academy, bepul dasturlash, rezyume yozish, portfolio, Shaxzod  javascript, Shaxzod  raqamli avlod, javascript, reactjs, vuejs, javascript darslari, reactjs darslari, vuejs darslari, dasturlash darslari, o'zbek tilida dasturlash, reactjs o'zbek tilida, reactjs darslari o'zbek tilida, javascript darslari, javascript darslari o'zbek tilida, dasturash darslari o'zbek tilida, dasturlashni o'rganish, dasturlash, IT loyihalar o'zbek tilida",
-		ogImage: 'https://media.graphassets.com/YpL2s0jESA2Z7EwBrh69',
+		ogImage: 'https://media.graphassets.com/NOD6BoMT8Gcn9eXQtNK6',
 		author: 'Shaxzod Aliyorov',
 	},
 	baseURL: 'https://shaxzod.netlify.app/',
