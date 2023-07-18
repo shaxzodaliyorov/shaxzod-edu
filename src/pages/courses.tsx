e29ab10871f76a88e241ba923774cf2068106325
shaxzod <shaxzodaliyorov0202@gmail.com>
2023-07-18 16:57:33 +0500

    ssr

--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -1,35 +1,50 @@
-import { GetServerSideProps } from 'next';
-import { useTranslation } from 'react-i18next';
-import { allCoursesType } from '../interfaces/courses';
-import { WithLayout } from '../layouts/layout';
-import Seo from '../layouts/seo/seo';
-import { CoursesPage } from '../page-components';
-import { GET_ALL_COURSES } from '../services/courses.services';
+import { GetServerSideProps } from "next";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { useAppDispatch } from "../hooks/redux";
+import { allCoursesType } from "../interfaces/courses";
+import { WithLayout } from "../layouts/layout";
+import Seo from "../layouts/seo/seo";
+import { CoursesPage } from "../page-components";
+import { GET_ALL_COURSES } from "../services/courses.services";
+import { getCourses, loadingCourse } from "../store/courses/course.slice";
 
-const Courses = ({}: CoursePageProps) => {
-	const { t } = useTranslation();
+const Courses = ({}) => {
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
 
-	return (
-		<Seo
-			metaTitle={`Shaxzod | ${t<string>('all_courses', { ns: 'seo' })}`}
-			metaDescription={t<string>('course_dic', { ns: 'seo' })}
-		>
-			<CoursesPage />
-		</Seo>
-	);
+  const getAllCourses = async () => {
+    dispatch(loadingCourse(true));
+    const response = await GET_ALL_COURSES.GET();
+    dispatch(getCourses(response));
+    dispatch(loadingCourse(false));
+  };
+
+  useEffect(() => {
+    getAllCourses();
+  }, []);
+
+  return (
+    <Seo
+      metaTitle={`Shaxzod | ${t<string>("all_courses", { ns: "seo" })}`}
+      metaDescription={t<string>("course_dic", { ns: "seo" })}
+    >
+      <CoursesPage />
+    </Seo>
+  );
 };
 
 export default WithLayout(Courses);
 
-export const getServerSideProps: GetServerSideProps<CoursePageProps> = async () => {
-	const courses = await GET_ALL_COURSES.GET();
-	return {
-		props: {
-			courses,
-		},
-	};
-};
+// export const getServerSideProps: GetServerSideProps<CoursePageProps> = async () => {
+// 	const courses = await GET_ALL_COURSES.GET();
+// 	return {
+// 		props: {
+// 			courses,
+// 		},
+// 	};
+// };
 
-export interface CoursePageProps extends Record<string, unknown> {
-	courses: allCoursesType[];
-}
+// export interface CoursePageProps extends Record<string, unknown> {
+// 	courses: allCoursesType[];
+// }
